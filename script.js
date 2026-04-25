c4181d3fd48d1c88a267d82bbd572a25b72e4d74
아루 <subil0306ab@gmail.com>
2026-04-26 01:31:10 +0900

    feat(js): 당첨 번호 함수 수정
    *feat
    로또 숫자 뽑는 개수 헷갈려서

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ function priceCalculate(price, quantity) {
 //2. 당첨 번호 생성, 자동 번호 선택 JS 함수 구현
 //흠 함수 중복이 있는 것 같은데 어케 해결하지
 
-function generateWinNumbers() {
+function generateRandomNumbers(count = 6) {
     //흠..중복이 없어야 하니까 set 저장할까
     const winNum = new Set();
-    while(winNum.size <7){
+    while(winNum.size <count){
         const randomNum =Math.floor(Math.random() * 45) + 1; //1~45까지의 랜덤 숫자 생성
         winNum.add(randomNum);
     }
     return [...winNum];
 }
 
-const randomSelectNum = generateWinNumbers();
-console.log(randomSelectNum);
-
+const winNumbers = generateRandomNumbers(7);
+const myNumbers = generateRandomNumbers(6);
 
 
