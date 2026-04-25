2d34d9c73f438fb2b9b462296aac24c7f77e9681
아루 <subil0306ab@gmail.com>
2026-04-26 00:03:02 +0900

    fix(js): 당첨 번호 생성 함수 수정 및 중복 함수 개선
    *fix
    1. 함수 중복 개선
    2. 보너스 숫자까지 뽑을 수 있도록 수정
    3. set 객체를 배열로 반환될 수 있도록 수정

--- script.js
+++ script.js
@@ -5,27 +5,20 @@ function priceCalculate(price, quantity) {
 }
 
 //2. 당첨 번호 생성, 자동 번호 선택 JS 함수 구현
+//흠 함수 중복이 있는 것 같은데 어케 해결하지
 
 function generateWinNumbers() {
     //흠..중복이 없어야 하니까 set 저장할까
     const winNum = new Set();
-    while(winNum.size <6){
+    while(winNum.size <7){
         const randomNum =Math.floor(Math.random() * 45) + 1; //1~45까지의 랜덤 숫자 생성
         winNum.add(randomNum);
     }
-    return winNum;
+    return [...winNum];
 }
 
-// console.log(generateWinNumbers());
+const randomSelectNum = generateWinNumbers();
+console.log(randomSelectNum);
 
-function randomSelectNum(){
-    const selectNum = new Set();
-    while(selectNum.size<6){
-        const randomNum = Math.floor(Math.random()*45)+1;
-        selectNum.add(randomNum);
-    }
 
-    return selectNum;
-}
 
-console.log(randomSelectNum())
