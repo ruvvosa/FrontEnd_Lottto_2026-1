94821da4f5ca2a548849276cea040dc51157815e
아루 <subil0306ab@gmail.com>
2026-04-26 16:38:03 +0900

    feat(js): 당첨 번호, 순위 및 금액 출력 기능 구현

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // 1. 로또 가격 계산 JS 함수 구현
+
 const TicketPrice = 1000;
-function priceCalculate(TicketPrice, quantity) {
-    return TicketPrice * quantity;
+function priceCalculate(TicketPrice, quanity) {
+    return TicketPrice * quanity;
 }
 
 //2. 당첨 번호 생성, 자동 번호 선택 JS 함수 구현
-//흠 함수 중복이 있는 것 같은데 어케 해결하지
 
 function generateRandomNumbers(count = 6) {
-    //흠..중복이 없어야 하니까 set 저장할까
     const winNum = new Set();
     while(winNum.size <count){
         const randomNum =Math.floor(Math.random() * 45) + 1; //1~45까지의 랜덤 숫자 생성
@@ -23,7 +22,6 @@ const myNumbers = generateRandomNumbers(6);
 
 
 //3. 당첨 순위 판정(내 번호 vs 당첨 번호)
-
 
 function ranking(winNumbers, myNumbers) {
     const bonusNum = winNumbers[6]; //보너스 번호
@@ -51,4 +49,19 @@ function prizeMoney(rank) {
         default: return 0;
     }
 }
+
+//실행
+
+const quanity = 5;
+const myTickets = Array.from({length:quanity},()=> generateRandomNumbers(6)); //로또 5장 뽑음
+const ranks = myTickets.map(ticket => ranking(winNumbers,ticket)); // 각 장의 등수
+const totalPrize = ranks.reduce((sum,rank)=> sum + prizeMoney(rank),0); //아 육회 먹고싶다
+
+console.log(`당첨 번호: ${winNumbers.slice(0,6)} 당첨 번호 ${winNumbers[6]}`);
+console.log(`구매 금액 ${priceCalculate(TicketPrice, quanity)}원`);
+console.log(`당첨 등수 ${ranks}`);
+console.log(`총 당첨금 ${totalPrize}원`);
+
+
+
 
